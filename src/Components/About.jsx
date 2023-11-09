e78a0f245e5a8ca14b0d7816e9c81819d05bf9a2
Clécio Bruno <77591613+cleciobruno@users.noreply.github.com>
2023-11-09 19:11:16 +0000

    produ

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,25 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/image2.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "code";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Sou um desenvolvedor focado no back-end, atuação em desenvolvimento de APIs: (NodeJs, Sql/NoSql), Análise de dados e aprendizagem de máquina: (Python), desenvolvimento de sites: (HTML, CSS, JS).";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "NodeJs",
+  "Sql e NoSql",
+  "Python",
+  "HTML, CSS, JS"
 ];
 
 /**
@@ -45,7 +43,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "Gosto de resolver problemas e desafios lógicos...";
 
 const About = () => {
   return (
@@ -60,7 +58,7 @@ const About = () => {
           textAlign: "center",
         }}
       >
-        <h2>About Myself</h2>
+        <h2>Sobre Mim</h2>
         <p className="large">{description}</p>
         <hr />
         <ul
